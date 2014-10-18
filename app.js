499d7d4a6c2883d5b4dfcb2ad9a07c1cb9447a02
Daniel Luna <dluna9081@gmail.com>
2014-10-18 00:13:26 -0700

    basic files

--- app.js
+++ app.js
@@ -10,12 +10,18 @@ var users = require('./routes/users');
 
 var app = express();
 
+var server = require('http').createServer(app);
+
+app.set('port', process.env.PORT || 3000);
+
+app.set('port', process.env.PORT || 3000);
+server.listen(app.get('port'));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
